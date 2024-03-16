e667ee0cdb4753cafee86ffd9e2f00ea2526ea52
tdavis24 <144742362+tdavis24@users.noreply.github.com>
2024-03-15 19:40:09 -0500

    Update and rename SEC05J.java to SEC05.java

--- src/rules/SEC05.java
+++ src/rules/SEC05.java
@@ -7,7 +7,7 @@
 
 import java.lang.reflect.Field;
 
-class SEC05J
+class SEC05
 {
     private int int1;
  
@@ -20,16 +20,16 @@ class SEC05J
 
     public static void main(String[] args)
     {
-        SEC05J sec05J = new SEC05J();
+        SEC05 sec05 = new SEC05();
 
         //Demonstrate how reflection can be used to access and change private variables
         try
         {
-            Field fieldInt1 = SEC05J.class.getDeclaredField("int1");
+            Field fieldInt1 = SEC05.class.getDeclaredField("int1");
             fieldInt1.setAccessible(true);
-            fieldInt1.setInt(sec05J, 42); //Changing the value using reflection
+            fieldInt1.setInt(sec05, 42); //Changing the value using reflection
 
-            System.out.println("Modified int1 using reflection: " + sec05J.int1);
+            System.out.println("Modified int1 using reflection: " + sec05.int1);
         }
         catch (NoSuchFieldException | IllegalAccessException e)
         {
