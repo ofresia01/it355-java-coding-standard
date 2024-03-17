69ffb4025ff40c7eb3c037bf34313ad0b0ae7070
FisherZE <125408455+FisherZE@users.noreply.github.com>
2024-03-17 14:58:44 -0500

    Add files via upload

---
+++ src/rules/Env05.java
@@ -0,0 +1,6 @@
+public class Env05 {
+    public static void main(String[] args) {
+        System.out.println("JMX remote monitoring is disabled by default.");
+        System.out.println("If enabled during development, it should be disabled before deployment.");
+    }
+}
