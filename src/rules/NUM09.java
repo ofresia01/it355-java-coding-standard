b902bbfe130c1cd9e87c622b74e8c430b8142991
jjyokup <128657754+jjyokup@users.noreply.github.com>
2024-03-18 01:07:45 +0000

    fixed executability of NUM09

--- src/rules/NUM09.java
+++ src/rules/NUM09.java
@@ -1,7 +1,7 @@
 //A class demonstrating utilizing ints over floats for iteration
 class NUM09{
     public static void main(String[] args){
-        float counter = 0.0;
+        float counter = 0;
         int x = 0;
         //The counter is swapped for an int before use in iteration
         int i = (int) counter;
